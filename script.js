0517895edba4650c7d2292117c7a5dc8a614892b
d43545047m-del <d43545047m@perueduca.edu.pe>
2026-09-22 14:29:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // 1. URL de la App de Google Apps Script donde se guardan los votos
-    const SCRIPT_URL = "URL_DE_TU_GOOGLE_APPS_SCRIPT_AQUI";
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzlM_LWSnbePRLDPV4fPBKdVzivIuPoG-JvGqF-tQV_2jaSX6RRSh_s33M6nZpuHtQ0fw/exec";
 
     // 2. Inicialización del Carrusel Swiper
     const swiper = new Swiper('.swiper-container', {
